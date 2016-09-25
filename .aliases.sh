2406e728e55585ba8fcfdcff3d6bbc8d8d8be19a
Arkadiusz Fal <arek@arekf.net>
2016-09-25 12:30:03 +0200

    Initial commit

---
+++ .aliases.sh
@@ -0,0 +1,44 @@
+alias cp='cp -iv'
+alias less='less -FSRXc'
+alias ls='ls -FGlhp'
+alias mkdir='mkdir -pv'
+alias mv='mv -iv'
+alias which='type -all'
+
+alias ..='cd ../'
+alias c='clear'
+alias edit='subl'
+alias mktar='tar -cvzf'
+alias pubkey='pbcopy < ~/.ssh/id_rsa.pub'
+alias size='du -sh *'
+
+# oh-my-zsh specific
+alias editaliases='subl ~/.aliases.sh'
+alias editprofile='subl ~/.zshrc'
+alias edittheme='subl ~/.oh-my-zsh/themes/arekf.zsh-theme'
+
+# macOS specific
+function lock_the_dock {
+    defaults write com.apple.Dock position-immutable -bool yes;
+    defaults write com.apple.Dock contents-immutable -bool yes;
+    defaults write com.apple.Dock size-immutable -bool yes;
+    killall Dock
+}
+
+function unlock_the_dock {
+    defaults write com.apple.Dock position-immutable -bool no;
+    defaults write com.apple.Dock contents-immutable -bool no;
+    defaults write com.apple.Dock size-immutable -bool no;
+    killall Dock
+}
+
+alias emptytrash='rm -rf ~/.Trash/*'
+alias f='open -a Finder ./'
+alias flushdnscache='sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;'
+
+# home stuff
+alias rpi='ssh arek@rpi'
+
+# work stuff
+alias previewized='ssh root@previewized1.xh'
+alias xh-fw='ssh -A xman@fw.xfive.co'
---
+++ zsh/arekf.zsh-theme
@@ -0,0 +1,15 @@
+function collapse_pwd {
+    echo $(pwd | sed -e "s,^$HOME,~,")
+}
+
+PROMPT='
+%{$fg_bold[green]%}$(collapse_pwd)%{$reset_color%} as %{$fg[magenta]%}%n%{$reset_color%} at %{$fg[cyan]%}%m%{$reset_color%}$(git_prompt_info)
+%{$fg[yellow]%}>%{$reset_color%} '
+
+RPROMPT='%{$fg[yellow]%}[$(date +"%H:%M:%S")]%{$reset_color%}'
+
+ZSH_THEME_GIT_PROMPT_PREFIX=" on %{$fg[yellow]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[green]%}"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[green]%}"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
